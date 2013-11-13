f323cf5a97f1e00e826e6a24d44c659aa8ffd592
Robert Taraya <robert.taraya@gmail.com>
2013-11-13 11:55:40 -0800

    Build database and model to capture Users. Added tweet form to allow tweet posts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,9 +17,7 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
   # at this point in the code is where you'll need to create your user account and store the access token
-
+  @user = User.create(@access_token.params)
   erb :index
-  
 end
--- db/migrate/20130410105729_create_users.rb
+++ db/migrate/20130410105729_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :screen_name
       t.string :oauth_token
-      t.string :oauth_secret
-
+      t.string :oauth_token_secret
+      t.string :user_id
       t.timestamps
     end
   end
