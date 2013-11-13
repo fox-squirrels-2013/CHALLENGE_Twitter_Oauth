335b7242127b322c3cb46063c1e9a46a49d6ba9b
Robert Taraya <robert.taraya@gmail.com>
2013-11-13 15:09:27 -0800

    tweeted to Bear-PUNCH

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,17 @@ get '/' do
   erb :index
 end
 
+post '/tweet/:id' do
+  # p params
+  content = params[:content]
+  user = User.find_by_id(params[:id])
+  p user
+  person = Twitter::Client.new(oauth_token: user.oauth_token, oauth_token_secret: user.oauth_token_secret)
+  person.update(content)
+  puts "You tweeted to the profile....I think"
+  erb :index
+end
+
 get '/sign_in' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   redirect request_token.authorize_url
@@ -16,8 +27,11 @@ get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
+  p session
+  puts "----------------------------"
   session.delete(:request_token)
   # at this point in the code is where you'll need to create your user account and store the access token
   @user = User.create(@access_token.params)
+  p session
   erb :index
 end
