7c1a304d7c204dd88d0a2f7003d9d67805cc9e24
Apprentice <apprentice@dbc10.local>
2013-11-13 12:14:27 -0800

    can tweet for usersgit status

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,7 @@
 get '/' do
+
+
+  @user = session[:user_data][:username] if session[:user_data]
   erb :index
 end
 
@@ -18,17 +21,39 @@ get '/auth' do
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
 
+
+user_data = {username: @access_token.params[:screen_name],
+             oauth_token: @access_token.params[:oauth_token],
+             oauth_secret: @access_token.params[:oauth_token_secret]}
+
 @user = User.find_by_username(@access_token.params[:screen_name])
 
 if @user.nil?
-
-  User.create username: @access_token.params[:screen_name],
-              oauth_token: @access_token.params[:oauth_token],
-              oauth_secret: @access_token.params[:oauth_token_secret]
+  User.create user_data
 end
 
+session[:user_data] = user_data
 
+# session[:oauth_token] = @access_token.params[:oauth_token]
+# session[:oauth_secret] = @access_token.params[:oauth_token_secret]
 
-  erb :index
+  redirect '/member'
+end
 
+get '/member' do
+  erb :member
+end
+
+post '/member' do
+
+  Twitter.configure do |config|
+    config.consumer_key = '9TDviFo0TFWqPwcBQ3O0tA'
+    config.consumer_secret = 'H3Jikhp8lrHgVw7Y78njB0ZFLordTjijdXSJknZoZg'
+    config.oauth_token = session[:user_data][:oauth_token]
+    config.oauth_token_secret = session[:user_data][:oauth_secret]
+  end
+
+  Twitter.update(params[:status])
+
+  redirect '/'
 end
